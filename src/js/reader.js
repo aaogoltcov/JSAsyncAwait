316d78d31e85d6438f541da76af2e05f6d8c2404
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-11-16 17:50:15 +0300

    init commit

---
+++ src/js/reader.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export default function read(data) {
+    return new Promise((resolve, reject) => {
+        // эмуляция чтения файла
+        setTimeout(() => {
+            return (input => {
+                const buffer = new ArrayBuffer(input.length * 2);
+                const bufferView = new Uint16Array(buffer);
+                for (let i = 0; i < input.length; i++) {
+                    bufferView[i] = input.charCodeAt(i);
+                }
+                resolve(buffer);
+            })(data);
+        }, 1000);
+    });
+}
